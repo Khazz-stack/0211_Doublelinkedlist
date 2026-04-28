a73f53e08ea43b142345ff3875c601f48dacc930
Khazz <thelenaid@gmail.com>
2026-04-28 14:28:01 +0700

    void addNode

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -20,4 +20,13 @@ public:
     {
         START = NULL;
     }
+
+    void addNode(){
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        
+
+    }
 };
